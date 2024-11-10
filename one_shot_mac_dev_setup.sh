b15f02c0ae9c3ac218b26b08ddc17235a7a7fe02
happydeveloper <enfn2001@gmail.com>
2024-11-10 09:11:50 +0900

    Xcode Command Line Tools 설치, Homebrew 설치

--- one_shot_mac_dev_setup.sh
+++ one_shot_mac_dev_setup.sh
@@ -3,3 +3,17 @@
 # Zsh 기본 쉘로 설정
 echo "Setting Zsh as the default shell..."
 chsh -s /bin/zsh
+
+# Xcode Command Line Tools 설치
+echo "Installing Xcode Command Line Tools..."
+xcode-select --install
+
+# Homebrew 설치
+if ! command -v brew &>/dev/null; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew is already installed"
+  echo "Current Homebrew version: $(brew --version | head -n 1)"
+fi
+
